5a24c69fecf78aab676c62988a9dbe7c8383520d
Kiranmayi Methuku <kmethuku@protonmail.com>
2021-05-27 17:32:15 -0700

    deleted tests

--- firebase.tsx
+++ firebase.tsx
@@ -21,25 +21,3 @@ if (!firebase.apps.length) {
 
 export const auth = app.auth();
 export default app;
-
-/*
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.4.2/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyD2IVs9jw6yL88VPsD3Rx7EikZPqfgDITg",
-    authDomain: "cardshare-3263b.firebaseapp.com",
-    projectId: "cardshare-3263b",
-    storageBucket: "cardshare-3263b.appspot.com",
-    messagingSenderId: "677993611062",
-    appId: "1:677993611062:web:8f41cb3230063545dffd97"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
-*/
--- globals.d.ts
+++
@@ -1,3 +0,0 @@
-import "@testing-library/jest-dom/extend-expect";
-import 'react-testing-library/cleanup-after-each';
-import 'jest-extended';
